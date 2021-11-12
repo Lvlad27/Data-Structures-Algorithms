08c20bf2ecbf6dc3b05fef55b53cf518007aa098
Vlad Leontescu <Lvlad27@gmail.com>
2021-11-12 18:41:14 +0200

    Solved two more, renamed references to boilerplat.txt

---
+++ 019. sortedUnion.js
@@ -0,0 +1,53 @@
+/*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+Check the assertion tests for examples.
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+
+uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+*/
+
+// MY METHOD
+
+function uniteUnique(arr) {
+    // Create an empty array in which we merge all the values from the input arrays.
+    let mergedArr = [];
+    // Loop through all of the input arrays
+    for (let i = 0; i < arguments.length; i++) {
+        // Merge all the values
+        mergedArr = mergedArr.concat(arguments[i]);
+    }
+    // Remove all duplicates from the array and assign them to a new final array.
+    let finalArr = [...new Set(mergedArr)];
+    return finalArr;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+/*
+METHOD 2
+function uniteUnique(...arrays) {
+    //make an array out of the given arrays and flatten it (using the spread operator)
+    const flatArray = [].concat(...arrays);
+  
+    // create a Set which clears any duplicates since it's a regular set and not a multiset
+    return [...new Set(flatArray)];
+  }
+  
+  // test here
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+  
+METHOD 3 (best)
+  function uniteUnique(...arr) {
+  return [...new Set(arr.flat())]
+}
+
+// Or as an arrow function
+const uniteUnique = (...arr) => [...new Set(arr.flat())]
+
+*/
---
+++ 020. convertHTMLEntities.js
@@ -0,0 +1,76 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana.
+convertHTML("Hamburgers < Pizza < Tacos") should return the string Hamburgers &lt; Pizza &lt; Tacos.
+convertHTML("Sixty > twelve") should return the string Sixty &gt; twelve.
+convertHTML('Stuff in "quotation marks"') should return the string Stuff in &quot;quotation marks&quot;.
+convertHTML("Schindler's List") should return the string Schindler&apos;s List.
+convertHTML("<>") should return the string &lt;&gt;.
+convertHTML("abc") should return the string abc.
+*/
+
+// MY METHOD
+function convertHTML(str) {
+    const entities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&apos;",
+    };
+    return str.replace(/&|<|>|'|"/g, (i) => entities[i]);
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+
+/*
+METHOD 2
+function convertHTML(str) {
+  // Split by character to avoid problems.
+  var temp = str.split("");
+
+  // Since we are only checking for a few HTML elements, use a switch
+  for (var i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join("");
+  return temp;
+}
+
+METHOD 3
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str
+    .split("")
+    .map(entity => htmlEntities[entity] || entity)
+    .join("");
+}
+
+*/
