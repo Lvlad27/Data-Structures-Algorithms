d9c5a03398275efc7cee4ae55f7e7a440f99068e
Vlad Leontescu <Lvlad27@gmail.com>
2021-12-08 00:09:46 +0200

    Updated

--- #tryOuts.js
+++ #tryOuts.js
@@ -1,4 +1,10 @@
-let arr = [
+let answer = {};
+let cash = 100;
+let price = 3.26;
+let changeTotal = cash - price;
+changeTotal = Math.round((changeTotal + Number.EPSILON) * 100) / 100;
+console.log(changeTotal);
+let cid = [
 	['PENNY', 1.01],
 	['NICKEL', 2.05],
 	['DIME', 3.1],
@@ -9,58 +15,64 @@ let arr = [
 	['TWENTY', 60],
 	['ONE HUNDRED', 100],
 ];
+let currency = [
+	['PENNY', 0.01],
+	['NICKEL', 0.05],
+	['DIME', 0.1],
+	['QUARTER', 0.25],
+	['ONE', 1],
+	['FIVE', 5],
+	['TEN', 10],
+	['TWENTY', 20],
+	['ONE HUNDRED', 100],
+];
+let change = [
+	['PENNY', 0],
+	['NICKEL', 0],
+	['DIME', 0],
+	['QUARTER', 0],
+	['ONE', 0],
+	['FIVE', 0],
+	['TEN', 0],
+	['TWENTY', 0],
+	['ONE HUNDRED', 0],
+];
 
-let sum = arr.reduce((sum, current) => sum + current[1], 0);
-sum = Math.round((sum + Number.EPSILON) * 100) / 100;
-
-const change = 96.74;
-
-function flatten(arr) {
-	let flatArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (Array.isArray(arr[i])) {
-			flatArr = flatArr.concat(flatten(arr[i]));
-		} else {
-			flatArr.push(arr[i]);
+const open = function () {
+	// Reverse cid array so that it will log the required order.
+	cid.reverse();
+	while (changeTotal > 0) {
+		for (i = 0; i < currency.length; i++) {
+			while (currency[i][1] <= changeTotal && cid[i][1] > 0) {
+				changeTotal -= currency[i][1];
+				console.log(changeTotal);
+				cid[i][1] -= currency[i][1];
+				console.log(cid[i][1]);
+				change[i][1] += currency[i][1];
+				console.log(change[i][1]);
+			}
 		}
+		return change;
 	}
-	return flatArr;
-}
+};
 
-console.log(flatten(arr));
-asdasda;
-arr = flatten;
-const newObject = Object.assign({}, arr);
-
-console.log(sum);
-console.log(change % 10);
+console.log(open());
 
 /*
-function open()
-checkCashRegister
-(3.26, 100, 
-    [
-        ["PENNY", 1.01], 
-        ["NICKEL", 2.05], 
-        ["DIME", 3.1], 
-        ["QUARTER", 4.25], 
-        ["ONE", 90], 
-        ["FIVE", 55], 
-        ["TEN", 20], 
-        ["TWENTY", 60], 
-        ["ONE HUNDRED", 100]]) 
-        should return 
-        {
-        status: "OPEN", 
-        change: 
-        [
-            ["TWENTY", 60], 
-            ["TEN", 20], 
-            ["FIVE", 15], 
-            ["ONE", 1], 
-            ["QUARTER", 0.5], 
-            ["DIME", 0.2], 
-            ["PENNY", 0.04]
-        ]
-        }
-*/
+cid.reverse();
+	for (let i in currency) {
+		while (change >= currency[i][1] && cid[i][1] > 0) {
+			changeArr.push(currency[i]);
+			cid[i][1] -= currency[i][1];
+			change -= currency[i][1];
+		}
+		if (change > currency[8][1]) {
+			answer['status'] = 'INSUFFICIENT_FUNDS';
+			answer['change'] = [];
+		} else {
+			answer['status'] = 'OPEN';
+			answer['change'] = changeArr;
+		}
+	}
+	return answer;
+    */
